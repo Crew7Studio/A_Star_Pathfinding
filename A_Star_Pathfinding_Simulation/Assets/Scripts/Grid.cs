ee452fc146423adac672cfb6381c82ff81971649
JojuGeorge <jojugeorge001@gmail.com>
2019-10-11 22:20:12 +0530

    Added code to find the position of gameobject placed on a node

--- A_Star_Pathfinding_Simulation/Assets/Scripts/Grid.cs
+++ A_Star_Pathfinding_Simulation/Assets/Scripts/Grid.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
+    // Test code
+    public Transform player;
+
+    
     // For creating the Grid
     [SerializeField] private Vector3 _gridWorldSize;
     [SerializeField] private LayerMask _unWalkableMasks;
@@ -23,15 +27,16 @@ public class Grid : MonoBehaviour
         _gridSizeX = Mathf.RoundToInt(_gridWorldSize.x / _nodeDiameter);     // To know how many grid we can place in X
         _gridSizeY = Mathf.RoundToInt(_gridWorldSize.z / _nodeDiameter);
 
-        print(_gridSizeX + " " + _gridSizeY);
 
         CreateGrid();
     }
 
 
+    // For creating the grid of nodes
     private void CreateGrid()
     {
-        _grid = new Node[_gridSizeX, _gridSizeY];
+        _grid = new Node[_gridSizeX, _gridSizeY];       // Setting the size of the array
+
         Vector3 worldBottomLeft = transform.position - Vector3.right * _gridWorldSize.x / 2 - Vector3.forward * _gridWorldSize.z / 2;   // To find the starting point to  create node. here bottom left
 
         for(int x = 0; x < _gridSizeX; x++)
@@ -41,9 +46,25 @@ public class Grid : MonoBehaviour
                 Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * _nodeDiameter + _nodeRadius) + Vector3.forward * (y * _nodeDiameter + _nodeRadius);
                 bool isWalkable = !(Physics.CheckSphere(worldPoint, _nodeRadius, _unWalkableMasks));
                 _grid[x, y] = new Node(isWalkable, worldPoint);
-                print(x+" "+y);
             }
         }
+    }
+
+
+
+    // For identifying the position in the gird
+    public Node NodeFromWorldPoint(Vector3 worldPositon)
+    {
+        float percentX = (worldPositon.x + _gridWorldSize.x / 2) / _gridWorldSize.x;        // Gets how far from the left most positin of the grid we are
+        float percentY = (worldPositon.z + _gridWorldSize.z / 2) / _gridWorldSize.z;
+
+        percentX = Mathf.Clamp01(percentX);
+        percentY = Mathf.Clamp01(percentY);
+
+        int x = Mathf.RoundToInt(percentX * (_gridSizeX - 1));          // -1 so that it wont go out of the array
+        int y = Mathf.RoundToInt(percentY * (_gridSizeY - 1));
+
+        return _grid[x, y];
     }
 
 
@@ -52,11 +73,17 @@ public class Grid : MonoBehaviour
         Gizmos.color = Color.green;
         Gizmos.DrawWireCube(transform.position, new Vector3(_gridWorldSize.x, 1f, _gridWorldSize.z));       // Drawing the Gridworld size
 
+        Node playerNode = NodeFromWorldPoint(player.position);
+
         if (_grid != null)
         {
             foreach (Node n in _grid)
             {
                 Gizmos.color = (n.isWalkable) ? Color.white : Color.red;
+                if (playerNode == n)
+                {
+                    Gizmos.color = Color.green;
+                }
                 Gizmos.DrawCube(n.worldPosition, Vector3.one * (_nodeDiameter - .1f));
             }
         }
